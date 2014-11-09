a3b53da5fd0a9f5752b844725bf79033cf981ce2
kuzhelov <deckster@mail.ru>
2014-11-09 15:38:58 +0000

    public assets system has been establishe

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 var http = require("http");
 var controllers = require('./controllers');
 
+// configuring routes
 controllers.init(app);
+
+// configuring view engine
 app.set('view engine', 'vash');
+
+// open public assets folder through middleware
+app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
 server.listen(process.env.PORT, process.env.IP);
